de0d4aba500b86b4d248ce28df4a1e1b840a5baf
Andrew Maxwell Fisher <anmfishe@crm.ucsc.edu>
2016-10-21 15:11:52 -0700

    Branches are looking pretty good. Next is making a lose condition and a win condition so at least something is turned in

--- Assets/Scripts/BranchScript.cs
+++ Assets/Scripts/BranchScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class BranchScript : MonoBehaviour {
     public GameObject childbranch;
+    public GameObject leaves;
+    public GameObject leaves2;
+    private GameObject trunk;
     private GameObject cb;
     private int numBraches = 0;
     private Rigidbody2D rb2d;
@@ -14,6 +17,28 @@ public class BranchScript : MonoBehaviour {
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         hj2d = GetComponent<HingeJoint2D>();
+        trunk = GameObject.FindGameObjectWithTag("Trunk");
+        GameObject lvs = (GameObject)Instantiate(leaves, transform.GetChild(0).transform.position, Quaternion.identity);
+        lvs.transform.parent = transform.parent;
+        HingeJoint2D lvhj2d = lvs.GetComponent<HingeJoint2D>();
+        lvhj2d.connectedBody = rb2d;
+        int numLeaves = (int)Random.Range(0f, 3f);
+        print(numLeaves);
+        if(numLeaves == 1)
+        {
+            GameObject lvs2 = (GameObject)Instantiate(leaves2, transform.GetChild(1).transform.position, Quaternion.identity);
+            lvs2.transform.parent = transform.parent;
+            HingeJoint2D lvhj2d2 = lvs2.GetComponent<HingeJoint2D>();
+            lvhj2d2.connectedBody = rb2d;
+        }
+        else if(numLeaves == 2)
+        {
+            GameObject lvs3 = (GameObject)Instantiate(leaves2, transform.GetChild(2).transform.position, Quaternion.identity);
+            lvs3.transform.parent = transform.parent;
+            HingeJoint2D lvhj2d3 = lvs3.GetComponent<HingeJoint2D>();
+            lvhj2d3.connectedBody = rb2d;
+
+        }
 	}
 	
 	// Update is called once per frame
@@ -34,30 +59,34 @@ public class BranchScript : MonoBehaviour {
     }
     void OnMouseDown()
     {
-        if (numBraches < 2 && !selecting)
+        if (numBraches < 3 && !selecting && transform.lossyScale.x > 0.07f)
         {
-            float randAngle = Random.Range(Mathf.PI/2f, -Mathf.PI/2f);
+            int randSide = (int)Random.Range(0, 2);
+            float randAngle = 0;
+
+            if (transform.rotation.z > 0f)
+            {
+
+                randAngle = Random.Range(Mathf.PI / 3f, 0);
+            }
+            else
+            {
+                randAngle = Random.Range(Mathf.PI / 6f, -Mathf.PI / 3f);
+                
+            }
           
+
+
             GameObject cb = (GameObject)Instantiate(childbranch, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.AngleAxis(randAngle * Mathf.Rad2Deg, Vector3.back));
-            //print(cb.transform.lossyScale.x + " " + cb.transform.lossyScale.y);
-            print(randAngle + " " + Mathf.Atan(randAngle));
-            cb.transform.localScale = new Vector3(transform.localScale.x * 0.7f, transform.localScale.y * 0.7f, transform.localScale.z);
-            cb.transform.position = new Vector3(cb.transform.position.x + cb.transform.lossyScale.y/2 *  Mathf.Atan(randAngle), cb.transform.position.y + cb.transform.lossyScale.y/2 * Mathf.Abs(Mathf.Cos(randAngle)), transform.position.z);
-            //cb.transform.rotation = Quaternion.AngleAxis(randAngle, Vector3.forward);
+            //print(cb.transform.rotation.z + " " + transform.rotation.z);
+            cb.transform.localScale = new Vector3(transform.localScale.x * 0.5f, transform.localScale.y * 0.6f, transform.localScale.z);
+            cb.transform.position = new Vector3(cb.transform.position.x + cb.transform.lossyScale.y/2 *  Mathf.Atan(randAngle), cb.transform.position.y + cb.transform.lossyScale.y/2 * Mathf.Abs(Mathf.Cos(randAngle)), transform.position.z - transform.lossyScale.z);
             HingeJoint2D hj2d2 = cb.GetComponent<HingeJoint2D>();
             hj2d2.connectedBody = rb2d;
-            /*JointAngleLimits2D jla2d = hj2d2.limits;
-            jla2d.min = randAngle - 5;
-            jla2d.max = randAngle + 5;
-            hj2d2.limits = jla2d;*/
-
             GameObject parent = transform.parent.gameObject;
             cb.transform.parent = parent.transform;
-            //cb.transform.rotation = Quaternion.AngleAxis(randAngle, Vector3.forward);
-
-            //rb2d.AddForce(new Vector2(100, 10));
+            transform.parent.transform.position = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y + .03f, transform.parent.transform.position.z);
             numBraches++;
-            //selecting = true;
         }
     }
 }
